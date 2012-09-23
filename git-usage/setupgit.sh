b76a00a420b17964f3e0f141bcc265865bc53b7e
whatacold <csuhgw@gmail.com>
2012-09-23 08:04:18 +0800

    replacing 1 tab with 4 spaces for indenting

--- git-usage/setupgit.sh
+++ git-usage/setupgit.sh
@@ -9,101 +9,101 @@ github_id=$githubdir/id_rsa.github
 
 test_github_key()
 {
-	echo -e "test your github key now...\n\
-		need you to add hithub to your known host list\n\
-		and type in your id's correspond password\n\
-		Finally check if it's succeeded with ssh's output yourself!"
-	if ! ssh -i $github_id git@github.com; then
-		echo "testing github key failed"
-		return
-	fi
+    echo -e "test your github key now...\n\
+    need you to add hithub to your known host list\n\
+    and type in your id's correspond password\n\
+    Finally check if it's succeeded with ssh's output yourself!"
+    if ! ssh -i $github_id git@github.com; then
+        echo "testing github key failed"
+        return
+    fi
 }
 
 gen_sshkeys_for_github()
 {
-	#XXX '[]' or test of shell don't recognise '~'
-	[ ! -d "$HOME/.ssh" ] && return
+    #XXX '[]' or test of shell don't recognise '~'
+    [ ! -d "$HOME/.ssh" ] && return
 
-	backupdir=~/.ssh/keys_before_github
-	mkdir -p "$backupdir"
-	cp -fp ~/.ssh/id_rsa* $backupdir
-	
-	mkdir -p $githubdir
+    backupdir=~/.ssh/keys_before_github
+    mkdir -p "$backupdir"
+    cp -fp ~/.ssh/id_rsa* $backupdir
 
-	echo "generating ssh keys for github now..."
-	echo "NEED you to type password manually!"
-	ssh-keygen -t rsa -C "$EMAIL" -f $github_id
+    mkdir -p $githubdir
 
-	echo "setting up ssh config..."
-	# if Hostname is not supported, then Host must specify as detail as possible
-	echo "Host github.com" >> ~/.ssh/config
-	echo -e "\tUser git" >> ~/.ssh/config
-	# Hostname keyword may be not supported
-	# Hostname github.com
-	echo -e "\tIdentityFile $github_id" >> ~/.ssh/config
-	#XXX mode must be set as 600, or you'll get error:
-	#Bad owner or permissions on /home/user/.ssh/config
-	#mode of /etc/ssh/ssh_config is 644
-	chmod 600 ~/.ssh/config
+    echo "generating ssh keys for github now..."
+    echo "NEED you to type password manually!"
+    ssh-keygen -t rsa -C "$EMAIL" -f $github_id
 
-	echo "keygen done, please paste your public id to github now..."
+    echo "setting up ssh config..."
+    # if Hostname is not supported, then Host must specify as detail as possible
+    echo "Host github.com" >> ~/.ssh/config
+    echo -e "\tUser git" >> ~/.ssh/config
+    # Hostname keyword may be not supported
+    # Hostname github.com
+    echo -e "\tIdentityFile $github_id" >> ~/.ssh/config
+    #XXX mode must be set as 600, or you'll get error:
+    #Bad owner or permissions on /home/user/.ssh/config
+    #mode of /etc/ssh/ssh_config is 644
+    chmod 600 ~/.ssh/config
+
+    echo "keygen done, please paste your public id to github now..."
 }
 
 config_git_globals()
 {
-	echo "configure git global settings now..."
-	# Sets the default name for git to use when you commit
-	git config --global user.name "$NAME"
+    echo "configure git global settings now..."
+    # Sets the default name for git to use when you commit
+    git config --global user.name "$NAME"
 
-	# Sets the default email for git to use when you commit
-	git config --global user.email "$EMAIL"
+    # Sets the default email for git to use when you commit
+    git config --global user.email "$EMAIL"
 
-	#Password cacheing
-	# Set git to use the credential memory cache
-	git config --global credential.helper cache
-	# Set the cache to timeout after 1 hour (setting is in seconds)
-	git config --global credential.helper 'cache --timeout=3600'
+    #Password cacheing
+    # Set git to use the credential memory cache
+    git config --global credential.helper cache
+    # Set the cache to timeout after 1 hour (setting is in seconds)
+    git config --global credential.helper 'cache --timeout=3600'
 }
 
 echo "configure git global settings[y/N]?"
 read answer
 case $answer in
-	y|Y)
-	config_git_globals
-	;;
-	n|N|"")
-	#nop
-	;;
-	*)
-	echo "treat your illegimate answer:$answer as NO"
-	;;
+    y|Y)
+    config_git_globals
+    ;;
+    n|N|"")
+    #nop
+    ;;
+    *)
+    echo "treat your illegimate answer:$answer as NO"
+    ;;
 esac
 
 echo "generate sshkey for github ?[y/N]?"
 read answer
 case $answer in
-	y|Y)
-	gen_sshkeys_for_github
-	;;
-	n|N|"")
-	#nop
-	;;
-	*)
-	echo "treat your illegimate answer:$answer as NO"
-	;;
+    y|Y)
+    gen_sshkeys_for_github
+    ;;
+    n|N|"")
+    #nop
+    ;;
+    *)
+    echo "treat your illegimate answer:$answer as NO"
+    ;;
 esac
 
 echo "test github key?[y/N]?"
 read answer
 case $answer in
-	y|Y)
-	test_github_key
-	;;
-	n|N|"")
-	#nop
-	;;
-	*)
-	echo "treat your illegimate answer:$answer as NO"
-	;;
+    y|Y)
+    test_github_key
+    ;;
+    n|N|"")
+    #nop
+    ;;
+    *)
+    echo "treat your illegimate answer:$answer as NO"
+    ;;
 esac
 
